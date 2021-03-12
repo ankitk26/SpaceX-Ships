54854c750de329a6a86f939a425c7d5ca48ad138
Ankit Kumar <myselfankit51@gmail.com>
2021-03-12 11:37:22 +0530

    added repo link in about page

--- pages/about.js
+++ pages/about.js
@@ -1,18 +1,28 @@
+import Link from "next/link";
 import Meta from "../components/Meta";
 
 const About = () => {
   return (
     <>
       <Meta title="About" />
-
       <span>This site uses SPACEX API to fetch the ships in SPACEX fleet</span>
+      <br />
+      <Link href="https://github.com/ankitk26/SpaceX-Ships" passHref={true}>
+        <a target="_blank" rel="noreferrer">
+          Github repo
+        </a>
+      </Link>
       <style jsx>
         {`
-          div {
-            width: 90%;
-            text-align: center;
-            margin: 0 auto;
-            margin-top: 2rem;
+          span {
+            font-size: 1.5rem;
+          }
+          a {
+            font-size: 1.2rem;
+            color: #005288;
+          }
+          a:hover {
+            text-decoration: underline;
           }
         `}
       </style>
